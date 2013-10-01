5aa5105fd4ee44233a22971d75e4b17455eda3eb
Riaan <mail@riaanlos.nl>
2013-10-01 10:36:24 +0200

    Update bookmark_bubble_msg_nl.js
    Added translations

--- bookmark_bubble_msg_nl.js
+++ bookmark_bubble_msg_nl.js
@@ -1,1 +1,20 @@
-// js code
+google.bookmarkbubble.Bubble.prototype.msg = {
+  android:
+    '<b>Gebruik als app:</b><br /> 1) Opslaan als bladwijzer,<br /> 2) Tik en houd de bladwijzer,<br /> 3) Selecteer "<b>Snelkoppeling naar startpagina toevoegen</b>"',
+  android3:
+    '<b>Gebruik als app:</b><br /> Kies <img src="'+ google.bookmarkbubble.Bubble.prototype.IMAGE_ANDROID3_BOOKMARK_DATA_URL_ +'" style="height: 1.5em;display: inline-block;padding:0;margin:0;" />,<br /> selecteer "<b>Toevoegen aan</b>" en dan "<b>Startpagina</b>"',
+  android4:
+    '<b>Gebruik als app:</b><br /> 1) Kies <img src="'+ google.bookmarkbubble.Bubble.prototype.IMAGE_ANDROID4_MOBILE_BOOKMARK_DATA_URL_ +'" style="height: 1.5em;display: inline-block;padding:0;margin:0;" />,<br /> 2) Selecteer "<b>Opslaan als bladwijzer</b>",<br /> 3) Selecteer "<b>Toevoegen aan</b>" en dan "<b>Beginscherm</b>"',
+  android41:
+    '<b>Gebruik als app:</b><br /> 1) Toevoegen als bladwijzer,<br /> 2) Tik en houd de bladwijzer,<br /> 3) Selecteer "<b>Snelkoppeling maken</b>"',
+  blackberry:
+    '<b>Gebruik als app:</b><br /> Kies <img src="'+ google.bookmarkbubble.Bubble.prototype.IMAGE_BLACKBERRY_ICON_DATA_URL_ +'" style="height: 1em;display: inline-block;padding:0;margin:0" /> en selecteer "<b>Voeg toe aan Beginscherm</b>"',
+  playbook:
+     '<b>Gebruik als app:</b><br /> Kies <img src="'+ google.bookmarkbubble.Bubble.prototype.IMAGE_PLAYBOOK_BOOKMARK_DATA_URL_ +'" style="height: 1.5em;display: inline-block;padding:0;margin:0;" /> en selecteer <br />"<b>Voeg toe aan Beginscherm</b>"',
+  ios42orlater :
+     '<b>Gebruik als app</b><br /> Kies <img src="'+ google.bookmarkbubble.Bubble.prototype.IMAGE_SAFARI_FORWARD_DATA_URL_ +'" style="height: 1em;display: inline-block;padding: 0;margin: 0" /> en daarna <b>"Voeg toe aan beginscherm"</b>',
+  ios7orlater :
+     '<b>Gebruik als app</b><br /> Kies <img src="'+ google.bookmarkbubble.Bubble.prototype.IMAGE_IOS7_FORWARD_DATA_URL_ +'" style="height: 2em;display: inline-block;padding: 0;margin: 0" /> en daarna <b>"Voeg toe aan beginscherm"</b>',
+  ioslegacy:
+     '<b>Gebruik als app</b><br /> Kies <b style="font-size:15px">+</b> en daarna <b>"Voeg toe aan beginscherm"</b>'
+};
